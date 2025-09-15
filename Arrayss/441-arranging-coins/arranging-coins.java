fca13ff6dcebb3e62cf7bc2fbaaaa07dfd30df86
anudeep4488 <anudeepj04@gmail.com>
2025-09-15 23:06:00 +0530

    Time: 1 ms (97.51%) | Memory: 40.9 MB (85.37%) - LeetSync

--- Arrayss/441-arranging-coins/arranging-coins.java
+++ Arrayss/441-arranging-coins/arranging-coins.java
@@ -1,13 +1,13 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int start=0,end=n;
+        int start=0,end=n;// defined start and end
 
-        while(start<=end){
+        while(start<=end){ // if definatly terminate start>end
 
-            int mid=start+(end-start)/2;
-            long requiredToFill =(long)mid*(mid+1)/2;
+            int mid=start+(end-start)/2;// find random number that number we are considerong as stairs which become n number we consider it as mic and find sum of natural number till that   number then we get how many conins retured that stair to fill it completly
+            long requiredToFill =(long)mid*(mid+1)/2;// calculate coins reuired  till that stair
 
-            if(requiredToFill==n){
+            if(requiredToFill==n){ //are specific stair is completly filled by givne number
                 return mid;
             }
             else if(requiredToFill>n){
@@ -18,7 +18,7 @@ class Solution {
             }
 
         }
-        return end;
+        return end; // we are retung end because it get at last completly filled stair and it terminate there it self (it make fli over there)
         
     }
 }
